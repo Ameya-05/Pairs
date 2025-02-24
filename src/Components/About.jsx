3de20aeab50fcf7333d94ffdb52675598ac10513
Ameya-05 <104995448+Ameya-05@users.noreply.github.com>
2025-02-24 10:57:49 +0530

    Changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import about from "../images/about.png";
+import about from "../images/About.png";
 
 gsap.registerPlugin(ScrollTrigger);
 
